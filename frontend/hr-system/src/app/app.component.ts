4575b514364b637977a1f0da52e7b865e9687d85
Tetak1389 <nikolaking88@gmail.com>
2024-11-12 14:57:01 +0100

    Created edit-profile component

--- frontend/hr-system/src/app/app.component.ts
+++ frontend/hr-system/src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginFormComponent } from './components/login/login-form/login-form.component';
 import { ResetPasswordFormComponent } from './components/login/reset-password-form/reset-password-form.component';
+import { EditProfileFormComponent } from './components/employees/edit-profile-form/edit-profile-form.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LoginFormComponent, ResetPasswordFormComponent],
+  imports: [RouterOutlet, LoginFormComponent, ResetPasswordFormComponent, EditProfileFormComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
